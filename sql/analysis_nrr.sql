676b637ab55ca0d391c84656cabd7c110f9123cf
Chanyoung <chanyoungpark0805@gmail.com>
2026-01-04 18:59:37 -0700

    -net revenue retention analysis -important SQL techniques/logics used: 1: cross join in making CTE tables and left join in the select statement. 2: the where condition in cross joins to select only active subscriptions at each time point.

---
+++ sql/analysis_nrr.sql
@@ -0,0 +1,31 @@
+use da_project_no1;
+select * from subscriptions;
+with recursive
+    months as (select last_day('2024-01-01') as month_end
+               union all
+               select last_day(DATE_ADD(month_end, INTERVAL 1 MONTH))
+               from months
+               where month_end < DATE('2024-12-01')),
+mrr_past as (
+    select month_end, account_id, sum(mrr_amount) as cohort_mrr
+    from months
+    cross join subscriptions
+    where start_date <= DATE_SUB(month_end, INTERVAL 12 MONTH) and (end_date is null or end_date >= DATE_SUB(month_end, INTERVAL 12 MONTH))
+    group by 1,2
+    ),
+mrr_current as (
+    select month_end, account_id, sum(mrr_amount) as current_mrr
+    from months
+    cross join subscriptions
+    where start_date <= month_end and (end_date is null or end_date >= month_end)
+    group by 1,2
+    )
+select mrr_past.month_end, sum(cohort_mrr) as cohort_mrr, sum(coalesce(current_mrr,0)) as current_mrr,
+       case when
+                sum(coalesce(cohort_mrr,0)) !=0 then concat(round(sum(current_mrr)*100/sum(cohort_mrr),2),'%')
+                else null end as nrr_rate
+    from mrr_past
+left join mrr_current
+on mrr_past.month_end = mrr_current.month_end and mrr_past.account_id = mrr_current.account_id
+group by 1
+order by month_end;
